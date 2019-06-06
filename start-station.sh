d4d25aa95d3ca2ac707f1be23b97ced62e237e41
strelly83 <strelly@teletu.it>
2019-06-06 14:32:10 +0200

    Delete start-station.sh

--- start-station.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# How To Autostart Apps In Rasbian LXDE Desktop
-# http://www.raspberrypi-spy.co.uk/2014/05/how-to-autostart-apps-in-rasbian-lxde-desktop/
-
-echo "Starting Weather Station"
-/usr/bin/python /home/pi/pisensehat/weather_station.py
