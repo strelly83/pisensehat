5f8eabcf59884b5a522ab23543d110af3900172b
strelly83 <strelly@teletu.it>
2019-06-06 22:10:18 +0200

    Update weather_station_ubi_therm.py

--- weather_station_ubi_therm.py
+++ weather_station_ubi_therm.py
@@ -38,39 +38,65 @@ dS = None
 # constants used to display an up and down arrows plus bars
 # modified from https://www.raspberrypi.org/learning/getting-started-with-the-sense-hat/worksheet/
 # set up the colours (blue, red, empty)
-b = [0, 0, 255]  # blue
-r = [255, 0, 0]  # red
-e = [0, 0, 0]  # empty
-# create images for up and down arrows
-arrow_up = [
-    e, e, e, r, r, e, e, e,
-    e, e, r, r, r, r, e, e,
-    e, r, e, r, r, e, r, e,
-    r, e, e, r, r, e, e, r,
-    e, e, e, r, r, e, e, e,
-    e, e, e, r, r, e, e, e,
-    e, e, e, r, r, e, e, e,
-    e, e, e, r, r, e, e, e
+
+number = [
+0,1,1,1, # Zero
+0,1,0,1,
+0,1,0,1,
+0,1,1,1,
+0,0,1,0, # One
+0,1,1,0,
+0,0,1,0,
+0,1,1,1,
+0,1,1,1, # Two
+0,0,1,1,
+0,1,1,0,
+0,1,1,1,
+0,1,1,1, # Three
+0,0,1,1,
+0,0,1,1,
+0,1,1,1,
+0,1,0,1, # Four
+0,1,1,1,
+0,0,0,1,
+0,0,0,1,
+0,1,1,1, # Five
+0,1,1,0,
+0,0,1,1,
+0,1,1,1,
+0,1,0,0, # Six
+0,1,1,1,
+0,1,0,1,
+0,1,1,1,
+0,1,1,1, # Seven
+0,0,0,1,
+0,0,1,0,
+0,1,0,0,
+0,1,1,1, # Eight
+0,1,1,1,
+0,1,1,1,
+0,1,1,1,
+0,1,1,1, # Nine
+0,1,0,1,
+0,1,1,1,
+0,0,0,1
 ]
-arrow_down = [
-    e, e, e, b, b, e, e, e,
-    e, e, e, b, b, e, e, e,
-    e, e, e, b, b, e, e, e,
-    e, e, e, b, b, e, e, e,
-    b, e, e, b, b, e, e, b,
-    e, b, e, b, b, e, b, e,
-    e, e, b, b, b, b, e, e,
-    e, e, e, b, b, e, e, e
-]
-bars = [
-    e, e, e, e, e, e, e, e,
-    e, e, e, e, e, e, e, e,
-    r, r, r, r, r, r, r, r,
-    r, r, r, r, r, r, r, r,
-    b, b, b, b, b, b, b, b,
-    b, b, b, b, b, b, b, b,
-    e, e, e, e, e, e, e, e,
-    e, e, e, e, e, e, e, e
+
+celcius_color = [255,0,0] # Red
+fahrenheit_color = [0,255,0] # Green
+negative_celcius_color = [0,255,255] # Cyan
+negative_fahrenheit_color = [0,0,255] # Blue
+empty = [0,0,0] # Black
+
+display = [
+0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0
 ]
 
 
@@ -235,17 +261,47 @@ def main():
                     now = datetime.datetime.now()
                     print("\n%d minute mark (%d @ %s)" % (MEASUREMENT_INTERVAL, current_minute, str(now)))
                     # did the temperature go up or down?
-                    if last_temp != temp_f:
-                        if last_temp > temp_f:
-                            # display a blue, down arrow
-                            sense.set_pixels(arrow_down)
-                        else:
-                            # display a red, up arrow
-                            sense.set_pixels(arrow_up)
-                    else:
-                        # temperature stayed the same
-                        # display red and blue bars
-                        sense.set_pixels(bars)
+                    
+                    while True:
+                        celcius = int(round(sense.get_temperature()))
+                        fahrenheit = int(round(1.8 * celcius + 32))
+
+                        if celcius < 0:
+                            celcius = abs(celcius)
+                            celcius_color = negative_celcius_color
+                         if fahrenheit < 0:
+                            fahrenheit = abs(fahrenheit)
+                            fahrenheit_color = negative_fahrenheit_color
+
+                         # Map digits to the display array
+                         pixel_offset = 0
+                         index = 0
+                         for index_loop in range(0, 4):
+                            for counter_loop in range(0, 4):
+                                display[index] = number[int(celcius/10)*16+pixel_offset]
+                                display[index+4] = number[int(celcius%10)*16+pixel_offset]
+                                display[index+32] = number[int(fahrenheit/10)*16+pixel_offset]
+                                display[index+36] = number[int(fahrenheit%10)*16+pixel_offset]
+                                pixel_offset = pixel_offset + 1
+                                index = index + 1
+                            index = index + 4
+
+                         # Color the temperatures
+                         for index in range(0, 64):
+                             if display[index]:
+                                if index < 32:
+                                    display[index] = celcius_color
+                                else:
+                                    display[index] = fahrenheit_color
+                             else:
+                                display[index] = empty
+
+                         # Display the temperatures
+                         sense.low_light = True # Optional
+                         sense.set_pixels(display)
+                    #    time.sleep(1)
+                    
+                    
                     # set last_temp to the current temperature before we measure again
                     last_temp = temp_f
 
